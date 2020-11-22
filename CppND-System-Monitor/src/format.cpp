e954c8a66531248936e59bfdf12d4b8929f35b01
MichaelGoldberg91 <mgold9191@gmail.com>
2020-11-22 21:59:18 +0000

    final submit

--- CppND-System-Monitor/src/format.cpp
+++ CppND-System-Monitor/src/format.cpp
@@ -5,10 +5,6 @@
 
 using std::string;
 
-// DONE: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
 
     //string variables
@@ -38,7 +34,7 @@ string Format::ElapsedTime(long seconds) {
         minutesString = "0" + minutesString;
     }
 
-    if (seconds < 10)
+    if (secondsOnly < 10)
     {
        secondsString = "0" + secondsString;
     }
